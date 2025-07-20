a22da47dcc9e78237239870bdfde5ee19d1ef32b
Grigogo <Grigoriirozhkov@gmail.com>
2025-07-20 15:30:44 +0300

    fix auth.service import

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,51 +1,73 @@
-import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {tap} from 'rxjs';
+import {Injectable, inject} from '@angular/core';
 import {ITokenResponse} from './auth.interface';
+import {catchError, tap, throwError} from 'rxjs';
 import {CookieService} from 'ngx-cookie-service';
 import {Router} from '@angular/router';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthService {
-  http: HttpClient = inject(HttpClient);
-  router = inject(Router);
+  http = inject(HttpClient)
+  router = inject(Router)
+  cookieService = inject(CookieService)
+  baseApiUrl = 'https://icherniakov.ru/yt-course/auth/'
 
-  baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
-  cookieService = inject(CookieService);
-
-  token: string | null = null;
-  refreshToken: string | null = null;
+  token: string | null = null
+  refreshToken: string | null = null
 
   get isAuth() {
-    return !!this.token;
+
+    if (!this.token) {
+      this.token = this.cookieService.get('token')
+      this.refreshToken = this.cookieService.get('refreshToken')
+    }
+    return (!!this.token)
   }
 
-  // get isAuth() {
-  //   console.log(this.token);
-  //   if (!this.token) {
-  //     this.token = this.cookieService.get('token');
-  //   }
-  //
-  //   return !!this.token;
-  // }
-
-  login(payload: { username: string; password: string }) {
+  login(payload: { username: string, password: string }) {
     const fd = new FormData();
-    console.log(fd)
 
-    fd.append('username', payload.username);
-    fd.append('password', payload.password);
+    fd.append('username', payload.username)
+    fd.append('password', payload.password)
 
-    return this.http.post<ITokenResponse>(`${this.baseApiUrl}token`, fd).pipe(
-      tap(val => {
-        this.token = val.access_token;
-        this.refreshToken = val.refresh_token;
+    console.log(payload)
+    return this.http.post<ITokenResponse>(
+      `${this.baseApiUrl}token`,
+      fd
+    ).pipe(
+      tap(val => this.saveTokens(val))
+    )
+  }
 
-        this.cookieService.set('token', this.token);
-        this.cookieService.set('refreshToken', this.refreshToken);
-      }),
-    );
+  refreshAuthToken() {
+    return this.http.post<ITokenResponse>(
+      `${this.baseApiUrl}refresh`,
+      {
+        refresh_token: this.refreshToken,
+      }
+    ).pipe(
+      tap(val => this.saveTokens(val)),
+      catchError(err => {
+        this.logout()
+        return throwError(err)
+      })
+    )
+  }
+
+  logout() {
+    this.cookieService.deleteAll()
+    this.token = null
+    this.refreshToken = null
+    this.router.navigate(['/login'])
+  }
+
+  saveTokens(res: ITokenResponse) {
+    this.token = res.access_token
+    this.refreshToken = res.refresh_token
+
+    this.cookieService.set('token', this.token)
+    this.cookieService.set('refreshToken', this.refreshToken)
   }
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,41 +1,37 @@
 import {Component, inject} from '@angular/core';
 import {
-    FormControl,
-    FormGroup,
-    ReactiveFormsModule,
-    Validators,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
-import {
-    AuthService
-} from '../../../../../../Documents/Tik-talk/src/app/auth/auth.service';
 import {Router} from '@angular/router';
 import {CookieService} from "ngx-cookie-service";
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
-    selector: 'app-login-page',
-    imports: [ReactiveFormsModule],
-    templateUrl: './login-page.component.html',
-    styleUrl: './login-page.component.scss',
+  selector: 'app-login-page',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
-    authService = inject(AuthService);
-    router = inject(Router);
-    cookieService = inject(CookieService);
+  authService = inject(AuthService);
+  router = inject(Router);
+  cookieService = inject(CookieService);
 
-    form = new FormGroup({
-        username: new FormControl<string | null>(null, Validators.required),
-        password: new FormControl<string | null>(null, Validators.required),
-    });
+  form = new FormGroup({
+    username: new FormControl<string | null>(null, Validators.required),
+    password: new FormControl<string | null>(null, Validators.required),
+  });
 
-    onSubmit() {
-        if (this.form.valid) {
+  onSubmit() {
+    if (this.form.valid) {
 
-            //@ts-ignore
-            this.authService.login(this.form.value).subscribe((res) => {
-                console.log('aaaaa');
-                this.router.navigate(['']);
-                console.log(res);
-            });
-        }
+      //@ts-ignore
+      this.authService.login(this.form.value).subscribe((res) => {
+        this.router.navigate(['']);
+      });
     }
+  }
 }
