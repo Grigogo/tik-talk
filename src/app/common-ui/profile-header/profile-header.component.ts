fe6580cf0815cb175d0fd215f1249fe0960441fd
Grigogo <grigoriirozhkov@gmail.com>
2025-08-05 09:36:53 +0300

    add avatar circle

--- src/app/common-ui/profile-header/profile-header.component.ts
+++ src/app/common-ui/profile-header/profile-header.component.ts
@@ -1,11 +1,13 @@
 import {Component, input} from '@angular/core';
 import {IProfile} from '../../data/interfaces/profile.interface';
 import {ImageUrlPipe} from '../../helpers/pipes/image-url.pipe';
+import {AvatarCircleComponent} from '../avatar-circle/avatar-circle.component';
 
 @Component({
   selector: 'app-profile-header',
   imports: [
-    ImageUrlPipe
+    ImageUrlPipe,
+    AvatarCircleComponent
   ],
   templateUrl: './profile-header.component.html',
   styleUrl: './profile-header.component.scss'
