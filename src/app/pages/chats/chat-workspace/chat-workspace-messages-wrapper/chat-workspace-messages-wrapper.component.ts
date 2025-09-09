0c45528c70cf02e691fc71f75614e6825deb0dc9
Grigogo <grigoriirozhkov@gmail.com>
2025-09-09 08:23:12 +0300

    add message date pipe

--- src/app/pages/chats/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component.ts
+++ src/app/pages/chats/chat-workspace/chat-workspace-messages-wrapper/chat-workspace-messages-wrapper.component.ts
@@ -2,12 +2,15 @@ import { Component, inject, input, signal } from '@angular/core';
 import { ChatWorkspaceMessageComponent } from './chat-workspace-message/chat-workspace-message.component';
 import { MessageInputComponent } from '../../../../common-ui/message-input/message-input.component';
 import { ChatsService } from '../../../../data/services/chats.service';
-import { IChat, IMessage } from '../../../../data/interfaces/chats.interface';
+import { IChat } from '../../../../data/interfaces/chats.interface';
 import { firstValueFrom } from 'rxjs';
+import {
+  GroupedMessageByDatePipe,
+} from '../../../../helpers/pipes/grouped-message-by-date.pipe';
 
 @Component({
   selector: 'app-chat-workspace-messages-wrapper',
-  imports: [ChatWorkspaceMessageComponent, MessageInputComponent],
+  imports: [ChatWorkspaceMessageComponent, MessageInputComponent, GroupedMessageByDatePipe],
   templateUrl: './chat-workspace-messages-wrapper.component.html',
   styleUrl: './chat-workspace-messages-wrapper.component.scss',
 })
@@ -15,7 +18,6 @@ export class ChatWorkspaceMessagesWrapperComponent {
   chatsService = inject(ChatsService);
 
   chat = input.required<IChat>();
-
   messages = this.chatsService.activeChatMessages;
 
   async onSendMessage(messageText: string) {
